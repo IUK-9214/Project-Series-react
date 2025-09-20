54b22d2cf196054dc303cec2bfdde5d53d881f54
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-09-19 23:38:56 -0700

    icons and some styling with functionality

--- Tree-Structure/src/component/Menu_list.jsx
+++ Tree-Structure/src/component/Menu_list.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Menu_Item from './Menu_Item'
 
-function Menu_list({list=[]}) {
-  return (
-    <ul className='menu-list-container'>
+function Menu_list({ list = [] }) {
+    return (
+        <ul className='' >
 
-        {list && list.length ?
-        
-        list.map(listItem=><Menu_Item item={listItem} />)
-        :null}
-    </ul>
-  )
+            {list && list.length ?
+
+                list.map(listItem => <Menu_Item item={listItem} />)
+                : null}
+        </ul>
+    )
 }
 
 export default Menu_list 
--- Tree-Structure/src/component/TreeView.jsx
+++ Tree-Structure/src/component/TreeView.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Menu_list from './Menu_list'
 
-function TreeView({menu=[]}) {
-  return (
-    <div className='tree-view-component'>
+function TreeView({ menu = [] }) {
+    return (
+        <div className='min-h-[100vh] w-[350px] bg-blue-700 '>
 
-        <Menu_list list={menu} />
-    </div>
-  )
+            <Menu_list list={menu} />
+        </div>
+    )
 }
 
 export default TreeView
