6a4541ec2bdf87631d1ed9ba6c05e9a797f0d422
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-09-19 23:40:08 -0700

    completed the tree structure

--- Tree-Structure/src/component/Menu_Item.jsx
+++ Tree-Structure/src/component/Menu_Item.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Menu_list from './Menu_list'
+import {FaPlus,FaMinus} from 'react-icons/fa'
 
-export default function Menu_Item({item}) {
+export default function Menu_Item({ item }) {
+
+
+  function handleToggleChildren(getcurrentlabel) {
+    setdisplaycurrentchildren({
+      ...displaycurrentchildren, [getcurrentlabel]: !displaycurrentchildren[getcurrentlabel]
+    })
+  }
+
+  const [displaycurrentchildren, setdisplaycurrentchildren] = useState({})
   return (
-    <li className='flex justify-center'>
+    <li className=' text-white font-bold uppercase pl-[30px]'>
 
-      <p>{item.label}</p>
+      <div className=' p-2 flex items-center gap-[20px] cursor-pointer'>
+        <p>{item.label}</p>
+        {item && item.children && item.children.length ?
+          <span onClick={() => handleToggleChildren(item.label)}>
 
-      {item && item.children && item.children.length >0 ? 
-       (<Menu_Item list={item.children} />)
-      :null}
+            {displaycurrentchildren[item.label] ? <FaMinus color='#fff' size={25}/> : <FaPlus color='#fff' size={25}/>}
+
+          </span>
+          : null}
+      </div>
+
+      {item && item.children && item.children.length > 0 && displaycurrentchildren[item.label] ?
+        (<Menu_list list={item.children} />)
+        : null}
     </li>
   )
 }
